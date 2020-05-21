6684a7a2d92542396beeab688a3e39cfc38dd9e3
sajidkhak <sajidkhaki@gmail.com>
2020-05-21 22:51:58 +0530

    implemented weather app api to fetch weather

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find weather information, temperature and more</p>
+    </div>
+)
+export default Title
---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Weather = props => {
+    return (
+        <div>
+            {props.city && <p>Location:  {props.city}</p>}
+            {props.country && <p>Country:  {props.country}</p>}
+            {props.temperature && <p>Temperature: {props.temperature}</p >}
+            {props.humidity && <p> Humidity: {props.humidity}</p >}
+            {props.description && <p>Conditions: {props.description}</p >}
+            {props.error && <p> {props.error} </p >}
+        </div>
+    )
+}
+export default Weather
