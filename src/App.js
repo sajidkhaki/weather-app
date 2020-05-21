78c64aa17fbee845b3d32da1d050816dc00d660a
sajidkhak <sajidkhaki@gmail.com>
2020-05-21 23:54:19 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -22,32 +22,52 @@ class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    console.log('data', data)
-    if (city && country) {
-      if (data.cod !=="404"){
+
+
+    try {
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+      const data = await api_call.json();
+      if (data) {
+        if (city && country) {
+          if (data.cod != "404") {
+            this.setState({
+              temperature: data.main.temp,
+              city: data.name,
+              country: data.sys.country,
+              humidity: data.main.humidity,
+              description: data.weather[0].description,
+              error: ""
+            });
+          } else {
+            this.setState({
+              temperature: undefined,
+              city: undefined,
+              country: undefined,
+              humidity: undefined,
+              description: undefined,
+              error: "Data not available for particular city"
+            })
+          }
+        } else {
+          this.setState({
+            temperature: undefined,
+            city: undefined,
+            country: undefined,
+            humidity: undefined,
+            description: undefined,
+            error: "Please enter the values."
+          })
+        }
+      } else {
+        console.log("cannot fetch")
+      }
+    } catch (error) {
+      console.log('Error', error)
       this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
-    }
-    } else {
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "Please enter the values."
-      });
+        error: "Please try again later"
+      })
     }
   }
-
   render() {
     return (
       <div>
